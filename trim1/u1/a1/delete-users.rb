ba6cad79f74f0d8924f3b5a6f55a0c071e9283dc
manuel <manuel@perez3.acosta>
2015-10-08 11:48:19 +0100

    Versión definitiva del script

--- trim1/u1/a1/delete-users.rb
+++ trim1/u1/a1/delete-users.rb
@@ -1,10 +1,11 @@
 # !/usr/bin/ruby 
 # encoding: utf-8
 
-resultado = `cat userslist.txt`
+resultado=`cat userslist.txt`
 
-users = resultado.split("\n")
+users=resultado.split("\n")
 
 users.each do |user|
 	system("userdel -r #{user}")
+	puts "Se ha borrado el usuario: #{user}"
 end
