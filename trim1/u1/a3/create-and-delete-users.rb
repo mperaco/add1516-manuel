bc5dd525d712dca8a52bb079f7bbe1158bbc6a11
manuel <manuel@perez3.acosta>
2015-10-20 10:50:29 +0100

    Avanzando en el script

--- trim1/u1/a3/create-and-delete-users.rb
+++ trim1/u1/a3/create-and-delete-users.rb
@@ -3,10 +3,28 @@
 
 user=`whoami`
 
-if user != "root"
+if user != "root\n"
 	puts "El usuario no es root"
 	exit
 end
 
 file=`cat userslist.txt`
-fils=file.split("\n")
+filas=file.split("\n")
+
+filas.each do |linea|
+	datos=linea.split(":")
+	
+	if datos[2] == ""
+		puts "El usuario #{datos[0]} no tiene email"
+	else 
+		if datos[-1] == "add"
+			system("useradd -m -s /bin/bash #{datos[0]}")
+			puts "Se ha creado el usuario #{datos[0]}"
+		else if datos[-1] == "delete"
+			system("userdel -r #{datos[0]}")
+			puts "Se ha borrado el usuario #{datos[0]}"
+		end
+		end
+	end
+
+end
