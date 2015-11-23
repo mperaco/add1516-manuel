596948a12178c4b80308ebe6e950d430683c2c35
manuel <mpacosta7@gmail.com>
2015-11-23 09:32:45 +0000

    Script terminado a falta de comprobaciones

--- trim1/u2/a2/install-and-remove.rb
+++ trim1/u2/a2/install-and-remove.rb
@@ -8,3 +8,29 @@ if user != "root\n"
 	exit
 end
 
+file=`cat software-list.txt`
+filas=file.slit("\n")
+
+filas.each do |linea|
+	datos=linea.split(":")
+	
+	comprobacion=`aptitude search #{datos[0]} | grep -w "^i   #{datos[0]}"`
+	
+	if datos[1] == "remove" || datos[1] == "r"
+		
+		if comprobacion == ""
+			puts "No se puede eliminar el paquete #{datos[0]} porque no está instalado."
+		else
+			system("apt-get -y purge #{datos[0]}")
+			puts "Se ha eliminado el paquete #{datos[0]} del sistema."
+	
+	elsif datos[1] == "install" || datos[1] == "i"
+		
+		if comprobacion == ""
+			system("apt-get -y install #{datos[0]}")
+			puts "Se ha instalado el paquete #{datos[0]} en el sistema."
+		else
+			puts "El paquetes #{datos[0]} ya está instalado."
+	end
+	end
+end
